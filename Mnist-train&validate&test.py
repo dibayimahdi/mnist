127a77f24690aa8d03f78bc13fe6a2094fd132ef
dibayimahdi <63470978+dibayimahdi@users.noreply.github.com>
2021-04-25 04:03:48 +0430

    Update and rename Mnist-2.py to Mnist-train&validate&test.py

--- Mnist-train&validate&test.py
+++ Mnist-train&validate&test.py
@@ -7,10 +7,12 @@ Created on Sat Apr 24 19:15:10 2021
 
 from keras.datasets import mnist
 import matplotlib.pyplot as plt
-
+import numpy as np
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
+
 from keras.losses import categorical_crossentropy
 #from keras.losses import 
 from keras.optimizers import SGD
@@ -32,21 +34,38 @@ Y_test=np_utils.to_categorical(test_labels)
 
 mymodel=Sequential()
 mymodel.add(Dense(500,activation='relu',input_shape=(784,)))
-
+mymodel.add(Dropout(0.2))
 mymodel.add(Dense(100,activation='relu'))
-
+mymodel.add(Dropout(0.2))
 mymodel.add(Dense(10,activation='softmax'))
 
 mymodel.summary()
 
 mymodel.compile(optimizer=SGD(lr=.001),loss='categorical_crossentropy', metrics=['accuracy'])
 
-network_history=mymodel.fit(X_train, Y_train,batch_size=128,epochs=3)
+network_history=mymodel.fit(X_train, Y_train,batch_size=128,epochs=3,validation_split=0.2)
 
 history=network_history.history
 acc=history['acc']
 loss=history['loss']
+val_acc=history['val_acc']
+val_loss=history['val_loss']
 
+
+plt.xlabel('epochs')
+plt.ylabel('acc')
 plt.plot(acc)
-plt.plot(loss)
+plt.plot(val_acc)
+plt.legend(['acc','val_acc'])
 
+plt.figure()
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.plot(loss)
+plt.plot(val_loss)
+
+plt.legend(['loss','val_loss'])
+
+mymodel.evaluate(X_test,Y_test)
+test_label_p=mymodel.predict(X_test)
+test_label_p = np.argmax(test_label_p, axis=1)
